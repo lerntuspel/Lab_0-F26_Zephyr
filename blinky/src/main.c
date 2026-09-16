06138731ccd2648a9acc08f472f124b2646a6457
AYU <alex.yu0753@gmail.com>
2026-09-16 17:59:34 -0400

    part 5.2 and 5.3

--- blinky/src/main.c
+++ blinky/src/main.c
@@ -10,38 +10,51 @@
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   2000
+#define POLL_MS 50
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED1_NODE DT_ALIAS(led5180)
+#define BUTTON1_NODE DT_ALIAS(button1)
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON1_NODE, gpios);
 
 int main(void)
 {
 	int ret;
-	bool led_state = true;
+	int last_state = 0;
 
-	if (!gpio_is_ready_dt(&led)) {
+	if (!gpio_is_ready_dt(&led) || !gpio_is_ready_dt(&button)) {
+		return 0;
+	}
+	
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
 	if (ret < 0) {
 		return 0;
 	}
 
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
+		int state = gpio_pin_get_dt(&button);
+
+		if (state < 0) {
+			k_msleep(POLL_MS);
+			continue;
 		}
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
+		if (state && !last_state) {
+			gpio_pin_toggle_dt(&led);
+		}
+
+		last_state = state;
+		k_msleep(POLL_MS);
 	}
 	return 0;
 }
